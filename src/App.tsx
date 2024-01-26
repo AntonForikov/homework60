d7898fd26f3be59fe4b13143b17434dd092e51c4
Anton Forikov <tohabass@gmail.com>
2024-01-26 21:09:39 +0600

    Add axios request function, install react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+// import {Button, Card} from "react-bootstrap";
+import {PostType} from "./types";
+import Post from "./components/Post/Post";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState<PostType[]>([]);
+
+  useEffect(() => {
+    void res();
+    // console.log(posts);
+  }, []);
+
+
+  const res = async () => {
+    try {
+      const response = await axios.get('http://146.185.154.90:8000/messages');
+      // console.log(response.data);
+      setPosts((prevState) => [...prevState, ...response.data]);
+      console.log(posts);
+    } catch {
+      alert('Please check requested URL.');
+    }
+  };
 
   return (
     <>
+      <Post />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
