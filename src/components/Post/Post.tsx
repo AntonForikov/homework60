f6f65faf0ecd4118b3b1ce4c431353eefe60c516
Anton Forikov <tohabass@gmail.com>
2024-01-27 12:46:24 +0600

    Post: Make humanreadable date

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -7,14 +7,26 @@ interface Props {
     date: string
 }
 const Post: React.FC<Props> = ({author, message, date}) => {
+    const dirtyDate = new Date(date);
+    const year = dirtyDate.getFullYear();
+    const month = dirtyDate.getMonth() + 1;
+    const day = dirtyDate.getDay();
+    const hour = dirtyDate.getHours();
+    const minutes = dirtyDate.getMinutes();
+
     return (
         <Card className='my-3 p-0'>
-            <Card.Header>Author: {author}</Card.Header>
+            <Card.Header>
+                <div>Author: {author}</div>
+                <div>{day < 10 ? `0${day}` : day}/{month < 10 ? `0${month}` : month}/{year}</div>
+            </Card.Header>
             <Card.Body>
                 <div className='d-flex justify-content-between align-items-center'>
-                    <Card.Title>Message: {message}</Card.Title>
+                    <Card.Title>
+                        Message: {message}
+                    </Card.Title>
                     <Card.Text>
-                        {date}
+                        {hour < 10 ? `0${hour}` : hour}:{minutes < 10 ? `0${minutes}` : minutes}
                     </Card.Text>
                 </div>
             </Card.Body>
