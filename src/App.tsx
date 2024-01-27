eadcaecf4d0415211f0337dced3217d684309c3b
Anton Forikov <tohabass@gmail.com>
2024-01-27 12:47:27 +0600

    App: reversed requested from API array

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,14 @@ function App() {
   useEffect(() => {
     setLoader(true);
     void res();
-    console.log('Posts:', posts);
   }, []);
 
   const res = async () => {
     try {
       const response = await axios.get('http://146.185.154.90:8000/messages');
-      setPosts((prevState) => [...prevState, ...response.data]);
+      const responseData = response.data;
+      const reversedData = responseData.reverse();
+      setPosts((prevState) => [...prevState, ...reversedData]);
       setLoader(false);
     } catch {
       alert('Please check requested URL.');
